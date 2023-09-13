7be14f417cfe269500e05f78eb71b1a2f7adeacd
085OrlovAE <sanyok123128@gmail.com>
2023-09-13 12:11:23 +0900

    Handled an exception that occurs when a character is not entered in the endpoint http://localhost:8081/api/v1/string/get/count/character

--- src/main/java/com/example/demotestworkt1/controllers/AdviceController.java
+++ src/main/java/com/example/demotestworkt1/controllers/AdviceController.java
@@ -2,6 +2,7 @@ package com.example.demotestworkt1.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -9,7 +10,11 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 @RestControllerAdvice
 public class AdviceController {
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<String> ConversionToSymbolFailed(MethodArgumentTypeMismatchException e) {
+    public ResponseEntity<String> handleConversionToSymbolFailed(MethodArgumentTypeMismatchException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<String> handleConversionToSymbolFailed(MissingServletRequestParameterException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
